a3074d188081161a043ef2b2ad4617d922220c75
wannarieff <wan_arief_hakemi@yahoo.com>
2021-05-02 22:57:17 +0800

    Edited datatograph
    Add header=None in line 5 so that kuala won't be skipped

--- Problem 2/DatatoGraph.py
+++ Problem 2/DatatoGraph.py
@@ -2,9 +2,9 @@ import pandas as pd
 import plotly.express as px
 import dash
 
-df = pd.read_csv('data.txt')
+df = pd.read_csv('data.txt', header=None)
 df.columns = ['Words','Frequency']
-df.to_csv('data.csv', index=False)
+df.to_csv('data.csv', index=None)
 df = pd.read_csv('data.csv')
 fig = px.bar(df, y='Frequency', x='Words')
 fig.update_traces(texttemplate='', textposition='outside')
